225d75d3798b43d323053fe9feca7fcfb1dc00b6
falcuun <falcuun@gmail.com>
2019-02-04 19:14:15 +0100

    Replacing all the vowels in a string with a given character

--- src/C73.java
+++ src/C73.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class C73 {
     public static void main(String[] arg)
     {
-        
+        String userInput, copiedString;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please input String you want copied: ");
+        userInput = scanner.nextLine();
+
+        copiedString = String.copyValueOf(userInput.toCharArray());
+
+        System.out.println("Value of Input is: " + userInput);
+        System.out.println("Value of Copied String is: " + copiedString);
     }
 }
--- src/C74.java
+++ src/C74.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class C74 {
     public static void main(String[] arg)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please Input string you want to encrypt: ");
+        String userInput = scanner.nextLine();
+        System.out.print("Please Input Special character you want to use for encryption: ");
+        String encryptionCharacter = scanner.nextLine();
+
+        String encryptedString = userInput.replaceAll("[AEIOUaeiou]", encryptionCharacter);
+
+        System.out.println("Original Input: " + userInput);
+        System.out.println("Encrypted Input: " + encryptedString);
     }
 }
