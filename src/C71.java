f43caf2a05889b682cdfdd95655c26f3084603bf
falcuun <falcuun@gmail.com>
2019-02-04 18:24:31 +0100

    Added a sentence reversal method

--- src/C71.java
+++ src/C71.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class C71 {
     public static void main(String[] arg)
     {
+        Scanner scanner = new Scanner(System.in);
 
+        String[] wordsOfSentence;
+
+        System.out.print("Please Enter A Sentence you want to reverse: ");
+        String userInput = scanner.nextLine();
+
+        wordsOfSentence = userInput.split(" ");
+
+        for(int i = wordsOfSentence.length - 1; i >= 0; i--){
+            System.out.print(wordsOfSentence[i] + " ");
+        }
     }
 }
