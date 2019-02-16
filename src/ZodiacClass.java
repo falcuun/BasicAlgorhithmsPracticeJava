8d89ec8277da03b67fa94f8c88c591135ad5507b
falcuun <falcuun@gmail.com>
2019-02-16 20:29:02 +0100

    Fixed named of the Zodiac Class

--- src/ZodiacClass.java
+++ src/ZodiacClass.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ZodiacClass {
-    public static void main(String[] args){
+    public void start(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please input your name: ");
         String name = scanner.nextLine();
